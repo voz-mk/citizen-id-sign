ac648e2f8940ecd7579b1f3205403ab3b9cf372d
a <a@sw.com>
2025-06-13 23:15:59 -0500

    pin input test for ios - 3

--- src/components/dnIe/PINCodeScreen.tsx
+++ src/components/dnIe/PINCodeScreen.tsx
@@ -57,7 +57,7 @@ const PINCodeScreen = ({ onNext }: PINCodeScreenProps) => {
                 placeholder="PIN"
                 value={pin}
                 onChange={(e) => handlePinInput(e.target.value)}
-                className="mx-auto w-2/3 text-center text-4xl tracking-wide"
+                className="mx-auto w-2/3 font-[Verdana] text-center text-4xl tracking-wide"
                 maxLength={6}
               />
               <button
